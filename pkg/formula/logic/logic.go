8f2778d661da52bf4a58be39d9093aeb18bc0ab8
Michael R. Krisnadhi <michaelkrisnadhi@gmail.com>
2025-07-05 20:18:03 +0700

    add support for inf & nan comparison

--- pkg/formula/logic/logic.go
+++ pkg/formula/logic/logic.go
@@ -3,11 +3,35 @@ package logic
 import "math"
 
 func Equ(x, y, epsilon float64) bool {
+	if math.IsInf(x, 1) {
+		return math.IsInf(y, 1)
+	}
+
+	if math.IsInf(x, -1) {
+		return math.IsInf(y, -1)
+	}
+
+	if math.IsInf(y, 1) {
+		return math.IsInf(x, 1)
+	}
+
+	if math.IsInf(y, -1) {
+		return math.IsInf(x, -1)
+	}
+
+	if math.IsNaN(x) {
+		return math.IsNaN(y)
+	}
+
+	if math.IsNaN(y) {
+		return math.IsNaN(x)
+	}
+
 	return math.Abs(x-y) <= epsilon
 }
 
 func NotEqu(x, y, epsilon float64) bool {
-	return math.Abs(x-y) > epsilon
+	return !Equ(x, y, epsilon)
 }
 
 func LTEqu(x, y, epsilon float64) bool {
